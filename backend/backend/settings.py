de157d27f3eb422d69626e4c085cb7ea9ea9c9b9
Prince Ofori Ankamah <prince.ankamah@amalitech.com>
2023-11-22 08:39:48 +0000

    Added new requirements

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@5644!n6#@1+cyh&8b*t64t4uqdolvw!0bu8sope)qhh3gs0wr'
+SECRET_KEY = os.environ.get("SECRET_KEY") 
+#'django-insecure-@5644!n6#@1+cyh&8b*t64t4uqdolvw!0bu8sope)qhh3gs0wr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
 
 AUTH_USER_MODEL = 'api.User'
 
@@ -98,8 +100,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
+database_url = os.environ.get("DATABASE_URL")
+DATABASES['default'] = dj_database_url.parse(database_url)
+# "postgres://lergershire_user:CebMmZLR6y8VVTBm5MCAL4m53oCqA0K1@dpg-cler2nl3qkas73avrap0-a.oregon-postgres.render.com/lergershire"
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
