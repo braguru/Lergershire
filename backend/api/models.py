afe51119e99de4db4021447bb02285727b81c087
Prince Ofori Ankamah <prince.ankamah@amalitech.com>
2023-11-23 10:23:56 +0000

    password reset

--- backend/api/models.py
+++ backend/api/models.py
@@ -115,7 +115,7 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
     """
     send_mail(
         # title:
-        "Password Reset for {title}".format(title="UpSkill portal account"),
+        "Password Reset for {title}".format(title="Lergershire password reset"),
         # message:
         email_plaintext_message,
         # from:
